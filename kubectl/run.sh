dec0132d7a656543ee850b5038ea831494ff7ca9
Vitaly Markov <vymarkov@users.noreply.github.com>
2019-04-16 13:33:07 +0300

    chore: Update kubectl/run.sh

--- kubectl/run.sh
+++ kubectl/run.sh
@@ -2,6 +2,10 @@
 # Run kubectl in a container
 
 # Inspired by https://docs.docker.com/compose/install/
+
+# $ sudo curl -L --fail https://raw.githubusercontent.com/lazyorangejs/vsode-in-docker/master/kubectl/run.sh -o /usr/local/bin/kubectl
+# $ sudo chmod +x /usr/local/bin/kubectl
+
 kubectl_ver=${KUBECTL_VERSION:-v1.14.0}
 
 kubectl () {
